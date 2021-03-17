797088c423ce0cddbfef34399e55e21491421a7d
haoyu0970624763 <haoyu0970624763@gmail.com>
2021-03-17 21:53:33 +0800

    Finishing the practice in class

--- unit/js/exercise.js
+++ unit/js/exercise.js
@@ -1,8 +1,20 @@
 // Step 3 code goes here
-
-
+let display = true
+$('.card:nth-child(1)').click(function(){
+  // Step 3: replace [alternative style] to an appropriate value
+  const style = display ? { opacity: 0.1 }
+  : { opacity:0.5 }
+  $(this).animate(style, 400)
+  display = !display
+})
 // Step 4 code goes here
 
+$('.card:nth-child(2)').click(function(){
+  $(this).toggleClass("card", 400) // Step 4: replace [css class] with an appropriate value
+})
 
 // Step 5 code goes here
 
+$('.card:nth-child(3)').click(function(){
+  $(this).transition('swing left')// Step 5: replace [effect] with an appropriate value
+})
