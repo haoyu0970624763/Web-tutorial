e2fed9cd5b9780496098e3de3d763da361e6e076
haoyu0970624763 <haoyu0970624763@gmail.com>
2021-03-18 15:37:35 +0800

    copy text from github

--- unit/http/ser.js
+++ unit/http/ser.js
@@ -1,5 +1,35 @@
-#!/usr/bin/env node
+/* Step 1:
+ * edit [port] to an appropriate value
+ * storing config to variables is a good practice, see `port` in the code
+ * learn the syntax of string interpolation in js, see `${port}` in the code
+ * 將 [port] 修改成合適的值
+ * 將設定放在變數中是一種好習慣，參考程式中的 `port`
+ * 學習 js 的 string interpolation 語法，參考程式中的 `${port}`
+ */
 
+// include `express`
+// 載入 `express`
+const express = require('express')
+
+// create an express, aka web server, instance
+// 建立一個 express (也就是網頁伺服器)實體
+const app = express()
+
+const port = 0857
+
+// handle `/step1` url
+// 處理 `/step1` 網址
+app.get('/step1', (req, res) => {
+  // response browser
+  // 回應瀏覽器
+  res.send('hello world')
+})
+
+// start the server
+// 啟動伺服器
+app.listen(port, () => {
+  console.log(`listening on port: ${0857}`)
+})
 // Step 1 and step 2 code goes here
 
 // Step 3 code goes here
@@ -7,5 +37,6 @@
 // Step 4 code goes here
 
 // Step 5 code goes here
+  // 回應瀏覽器
 
 // Step 7 code goes here
