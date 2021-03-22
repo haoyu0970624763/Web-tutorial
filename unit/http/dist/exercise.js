86783d81ee8bb77990f95d8bc815e8f6e4e341c2
haoyu0970624763 <haoyu0970624763@gmail.com>
2021-03-22 17:07:52 +0800

    Finish practice in the class

--- unit/http/dist/exercise.js
+++ unit/http/dist/exercise.js
@@ -1,10 +1,18 @@
 $(document).ready(function() {
-  $('#ajax-form button[type="submit"]').click((event) => {
-    event.preventDefault()
-    // Step 9 and step 10 code goes here
+	$('#ajax-form button[type="submit"]').click((event) => {
+		event.preventDefault()
+		// Step 9 and step 10 code goes here
+		$.get('./step5', {
+			fname: $('#ajax-form [name="fname"]').val(),
+			lname: $('#ajax-form [name="lname"]').val(),
+		}, (data) => {
+			$('#ajax-output').html(data)
+		})
 
-    $.get('./step5')
-
-    // Step 11 code goes here
-  })
+		// Step 11 code goes here
+		setTimeout(() => {
+			$('#ajax-output').html('loaded')
+		}, 1000)
+		$('#ajax-output').html('loading')
+	})
 });
